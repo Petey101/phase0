b6846fe1b9bc907d247209f421af1aa8b6b92e23
Petey101 <pleong101@live.com>
2016-01-20 19:06:52 -0500

    Initial Commit

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,8 @@
+/* As a user, I want to create a three functions that can take a group or numbers with an unknown length and do three things to them.
+I want to be able to take the sum of all the numbers in the group with one function.
+I want to be able to take the mean of all the numbers in the group with another function.
+Lastly I want to take the median of all the numbers in the group with a final function. 
+I want the functions to be named sum, mean, and median respectively.
+Remember that the length of these numbers may be of varying sizes so take that in mind.
+This last point will affect the median function mostly.
+*/
